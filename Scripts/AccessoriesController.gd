ae686241085ac186f5c2c7d72747407b714e6d4c
Martin Czuchra <martin.czuchra@googlemail.com>
2024-01-20 19:37:14 +0100

    working toggling of accessories

---
+++ Scripts/AccessoriesController.gd
@@ -0,0 +1,39 @@
+extends PanelContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_earrings_pressed():
+	if $Earrings.visible == true:
+		$Earrings.visible = false
+	else:
+		$Earrings.visible = true
+
+
+func _on_glasses_pressed():
+	if $Glasses.visible == true:
+		$Glasses.visible = false
+	else:
+		$Glasses.visible = true
+
+
+func _on_tail_trinket_pressed():
+	if $TailTrinket.visible == true:
+		$TailTrinket.visible = false
+	else:
+		$TailTrinket.visible = true
+
+
+func _on_crown_pressed():
+	if $Crown.visible == true:
+		$Crown.visible = false
+	else:
+		$Crown.visible = true
